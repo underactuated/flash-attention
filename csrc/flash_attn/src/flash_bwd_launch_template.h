0d611f8e22d5a67d90f34304fd93eb887284e4d3
Sergey Pankov <sergey.pankov@gmail.com>
2026-01-15 22:27:21 +0000

    tested backward()

--- csrc/flash_attn/src/flash_bwd_launch_template.h
+++ csrc/flash_attn/src/flash_bwd_launch_template.h
@@ -160,7 +160,7 @@ void run_mha_bwd_hdim32(Flash_bwd_params &params, cudaStream_t stream) {
 
 template<typename T, bool Is_causal>
 void run_mha_bwd_hdim64(Flash_bwd_params &params, cudaStream_t stream) {
-    /*
+    ///*
     constexpr static int Headdim = 64;
     int device;
     cudaGetDevice(&device);
@@ -179,7 +179,8 @@ void run_mha_bwd_hdim64(Flash_bwd_params &params, cudaStream_t stream) {
         // run_flash_bwd<Flash_bwd_kernel_traits<Headdim, 128, 64, 8, 4, 2, 4, false, false, T>, Is_dropout>(params, stream);
         // This is slightly faster. We want to split M more so we need fewer registers to store LSE.
         if (max_smem_per_block >= 144 * 1024) {
-            run_flash_bwd<Flash_bwd_kernel_traits<Headdim, 128, 128, 8, 4, 4, 4, false, false, T>, Is_dropout, Is_causal>(params, stream);
+            run_flash_bwd<Flash_bwd_kernel_traits<Headdim, 128, 64, 8, 4, 4, 4, false, false, T>, Is_dropout, Is_causal>(params, stream); // my test
+            // run_flash_bwd<Flash_bwd_kernel_traits<Headdim, 128, 128, 8, 4, 4, 4, false, false, T>, Is_dropout, Is_causal>(params, stream);
             // This has a lot of register spilling
             // run_flash_bwd<Flash_bwd_kernel_traits<Headdim, 128, 128, 8, 4, 4, 4, true, false, T>, Is_dropout>(params, stream);
         } else {
@@ -202,7 +203,7 @@ void run_mha_bwd_hdim64(Flash_bwd_params &params, cudaStream_t stream) {
     // run_flash_bwd<Flash_bwd_kernel_traits<Headdim, 64, 64, 4, false, T>>(params, stream);
 
     // run_flash_bwd<Flash_bwd_kernel_traits<Headdim, 128, 64, 4, 4, 2, 4, false, false, T>>(params, stream);
-    */
+    //*/
 }
 
 template<typename T, bool Is_causal>
--- csrc/flash_attn/src/softmax.h
+++ csrc/flash_attn/src/softmax.h
@@ -2117,7 +2117,7 @@ struct Softmax_c : public Softmax<kNRows> {
         //*/
         //sss.store_wws(row_max, row_sum);
         //sss.analyze_scores(scores, row_max(0));
-        sss.store_wws(row_max, row_sum, g_row_sum);
+        //sss.store_wws(row_max, row_sum, g_row_sum); // uncomment for weight storing
         //sss.original_coordinates(acc_s);
     };
 
